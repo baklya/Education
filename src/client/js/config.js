e7c2f68908213e78c20e3f1271284469d12269c2
Vladimir <you@example.com>
2015-08-05 02:24:09 +0300

    added jquery and d3

--- src/client/js/config.js
+++ src/client/js/config.js
@@ -1,8 +1,25 @@
 requirejs.config({
 
-    "shim": {
-        "jquery.alpha": ["jquery"],
-        "jquery.beta": ["jquery"]
+	paths: {
+		'jquery': '../../../bower_components/jquery/dist/jquery'
+	},
+    //Remember: only use shim config for non-AMD scripts,
+    //scripts that do not already call define(). The shim
+    //config will not work correctly if used on AMD scripts,
+    //in particular, the exports and init config will not
+    //be triggered, and the deps config will be confusing
+    //for those cases.
+    shim: {
+        'backbone': {
+            //These script dependencies should be loaded before loading
+            //backbone.js
+            deps: ['underscore', 'jquery'],
+            //Once loaded, use the global 'Backbone' as the
+            //module value.
+            exports: 'Backbone'
+        },
+        'underscore': {
+            exports: '_'
+        }
     }
 });
-
--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,11 +1,12 @@
 
 
 require(
-    ['modules/myModule'],
-    function( myModule ){
+    ['modules/myModule', 'jquery'],
+    function( myModule, jq ){
 
     	var omg = new myModule();
 
     	console.log(omg);
+    	console.log(jq);
     }
 );
