42e5da52cb6fae3072fb00432e42d22c13fb0d3c
Vladimir Matveev <donald151@gmail.com>
2015-08-06 15:22:21 +0000

    blue square

--- src/client/js/modules/Target.js
+++ src/client/js/modules/Target.js
@@ -1,13 +1,29 @@
-define(['d3', 'jquery'], function(d3, jq){
- 
-  var module = function(container) {
+define(['d3', 'jquery'], function(d3, jq) {
 
-    console.log("===");
-    console.log(jq);
-    console.log("===");
-    this.foo = "bar";
-    
+  var module = function(svg) {
+
+
+    var angle = 0;
+    var delta = 2 * Math.PI / 1000;
+
+    var mySquare = svg.append("rect")
+      .attr("x", (60 + 40 * Math.cos(angle)))
+      .attr("y", (60 + 40 * Math.sin(angle)))
+      .attr("width", 20)
+      .attr("height", 20)
+      .style("fill", "blue");
+
+    //console.log("===");
+    //console.log(jq);
+    //console.log("===");
+    this.SetNextPosition = function(){
+      angle += delta;
+      mySquare
+        .attr("x", (60 + 40 * Math.cos(angle)))
+        .attr("y", (60 + 40 * Math.sin(angle)));
+    };
+
   };
- 
+
   return module;
 });
--- src/client/js/modules/paintModule.js
+++ src/client/js/modules/paintModule.js
@@ -1,17 +1,42 @@
-define(['d3', 'jquery'], function(d3, jq){
- 
+define(['d3', 'jquery', 'modules/Target'], function(d3, jq, t) {
+
   var module = function(container) {
 
     var svg = d3.select(container).append("svg")
-		    .attr("width", "100%")
-		  .append("g");
-		    
+      .attr("width", "100%")
+      .append("g");
+
+    var showTime = svg.append('text')
+      .attr("x", 20)
+      .attr("y", 20)
+      .attr("fill", "red")
+      .text("Hello");
+
+    var target = new t(svg);
+
+    var prev = performance.now();
+    var times = 0;
+
+    requestAnimationFrame(function measure(time) {
+      //document.body.insertAdjacentHTML("beforeEnd", Math.floor(time - prev) + " ");
+      prev = time;
+    
+      showTime.text(time + "");
+    
+    
+      target.SetNextPosition();
+      
+    
+      requestAnimationFrame(measure);
+      //if (times++ < 10) requestAnimationFrame(measure);
+    })
+
     console.log("===");
     console.log(jq);
     console.log("===");
     this.foo = "bar";
-    
+
   };
- 
+
   return module;
 });
