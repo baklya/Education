36687cdafc52019d4369335095976f8865f99477
Vladimir Matveev <donald151@gmail.com>
2015-08-06 07:28:23 +0000

    added paint module

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,12 +1,18 @@
 
 
+
+
+
 require(
-    ['modules/myModule', 'jquery', 'd3'],
-    function( myModule, $, d3 ){
+    ['modules/paintModule', 'jquery', 'd3'],
+    function( paintModule, $, d3 ){
 
-    	var omg = new myModule();
 
-    	console.log(omg);
+
+
+    	var pm = new paintModule("body");
+
+    	console.log(pm);
     	console.log($);
 
     	console.log(d3);
@@ -16,11 +22,6 @@ require(
 		console.log(window);
 		
 
-		var svg = d3.select("body").append("svg")
-		    .attr("width", "100%")
-		  .append("g")
-		    
 
-    
     }
 );
